ce851782a5ac069071bfc4eeef24c28850aa63dd
Melis <59087686+melis@users.noreply.github.com>
2021-03-28 23:05:37 +0600

    progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#FFD700"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="2"
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
